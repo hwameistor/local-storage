1f52eca41b7fecf23fe05b90dfb6a8d23ad54584
123 <123>
2022-03-28 16:55:04 +0800

    e2e-fix-15-10

--- test/e2e/SmokeTest/lsHaVolume.go
+++ test/e2e/SmokeTest/lsHaVolume.go
@@ -21,7 +21,7 @@ import (
 	"time"
 )
 
-var _ = ginkgo.Describe("test localstorage Ha volume", ginkgo.Label("smokeTest"), func() {
+var _ = ginkgo.Describe("test localstorage Ha volume", ginkgo.Label("e2e"), func() {
 
 	f := framework.NewDefaultFramework(lsv1.AddToScheme)
 	client := f.GetClient()
