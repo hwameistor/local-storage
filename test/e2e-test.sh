7710888967ac2ade6d5fa30fe4a8b56d27bfbe6d
123 <123>
2022-04-01 18:12:42 +0800

    e2e-fix-17-7

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -13,8 +13,8 @@ export snapshot="e2etest"
 # govc find . -type m -runtime.powerState poweredOn
 # govc find . -type m -runtime.powerState poweredOn | xargs govc vm.info
 # govc vm.info $hosts
-echo "output username "
-echo ${GOVC_USERNAME}
+echo "GOVC_URL: "
+echo ${GOVC_URL}
 set -e
 for h in $hosts; do
   if [[ `govc vm.info $h | grep poweredOn | wc -l` -eq 1 ]]; then
