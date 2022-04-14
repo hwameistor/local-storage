6e49240c226c45b128cf9da7e34c061b549d16c3
123 <123>
2022-04-14 15:22:47 +0800

    Add deduplication test

--- test/e2e/SmokeTest/types.go
+++ test/e2e/SmokeTest/types.go
@@ -1,6 +1,7 @@
 package SmokeTest
 
 const (
-	DeploymentName   string = "e2e-2048"
-	HaDeploymentName string = "e2e-2048-ha"
+	DeploymentName             string = "e2e-2048"
+	HaDeploymentName           string = "e2e-2048-ha"
+	NumberOfDeduplicationTests int    = 30
 )
