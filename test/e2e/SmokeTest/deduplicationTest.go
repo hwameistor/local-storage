56db0490dba21dfb1bc4e30cfdf92fdaf8538975
123 <123>
2022-04-14 16:48:29 +0800

    Add deduplication test

--- test/e2e/SmokeTest/deduplicationTest.go
+++ test/e2e/SmokeTest/deduplicationTest.go
@@ -20,7 +20,7 @@ import (
 	"time"
 )
 
-var _ = ginkgo.Describe("test localstorage Ha volume", ginkgo.Label("pr"), func() {
+var _ = ginkgo.Describe("test localstorage Ha volume", ginkgo.Label("test"), func() {
 
 	f := framework.NewDefaultFramework(lsv1.AddToScheme)
 	client := f.GetClient()
